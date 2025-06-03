c7b94d402d1085d109187b6c665e296142e1686e
Adama COULIBALY <coulibalyadama1573@gmail.com>
2025-06-03 20:21:23 +0100

    update app

--- app.py
+++ app.py
@@ -1,30 +1,60 @@
-import torch
-from transformers import pipeline
+import whisper
 import gradio as gr
+import datetime
+import torch
 
-# Initialize the speech recognition pipeline
-pipe = pipeline(
-    "automatic-speech-recognition",
-    model="openai/whisper-small",
-    chunk_length_s=30,
-    device=-1,  # Use -1 for CPU, or specify a GPU device ID (e.g., 0) if available
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+model = whisper.load_model('base')
+
+translation_tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
+translation_model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M")
+translation_model.eval()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+translation_model.to(device)
+
+def transcribe(inputs , timestamp):
+    if inputs is None:
+          raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
+    output = ""
+    result = model.transcribe(inputs)
+    if timestamp == "Yes":
+      for indx, segment in enumerate(result['segments']):
+        output += str(datetime.timedelta (seconds=segment['start'])) +" "+ str(datetime.timedelta (seconds=segment['end'])) + "\n"
+        output += segment['text'].strip() + '\n'
+    else:
+      output = result["text"]
+
+    return  output
+
+# Fonction de traduction
+def translate(text):
+    inputs = translation_tokenizer(text, return_tensors="pt").to(device)
+    lang_id = translation_tokenizer.convert_tokens_to_ids("eng_Latn")
+    translated_tokens = translation_model.generate(
+        **inputs, forced_bos_token_id=lang_id, max_length=64
+    )
+    return translation_tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+
+# Fonction principale pour Gradio
+def process(audio, timestamp):
+    transcription = transcribe(audio, timestamp)
+    translation = translate(transcription)
+    return transcription, translation
+
+
+interface = gr.Interface(
+    fn=process,
+    inputs=[gr.Audio(sources=["upload"],type="filepath"),
+            gr.Radio(["Yes", "No"], label="Timestamp", info="Displays with timestamp if needed."),],
+    outputs=[
+        gr.Textbox(label="Transcription (Français)"),
+        gr.Textbox(label="Traduction (Anglais)")
+    ],
+    title="Audio Transcription And Translation App FR->GB",
+    description=(
+        "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the OpenAI Whisper API"
+    )
 )
 
-# Function to transcribe audio using the OpenAI Whisper model
-def transcript_audio(audio_file):
-    # Transcribe the audio file and return the result
-    result = pipe(audio_file, batch_size=8)["text"]
-    return result
-
-# Set up Gradio interface
-audio_input = gr.Audio(sources="upload", type="filepath")  # Audio input
-output_text = gr.Textbox()  # Text output
-
-# Create the Gradio interface with the function, inputs, and outputs
-iface = gr.Interface(fn=transcript_audio, 
-                     inputs=audio_input, outputs=output_text, 
-                     title="Audio Transcription",
-                     description="This is a simple web app for audio transcription (English-only) using Whisper model from OpenAI.")
-
-# Launch the Gradio app
-iface.launch()
+interface.launch()
